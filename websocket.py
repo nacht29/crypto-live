dd7b00e57324ea89c9240e9bb286f5379d85fc95
nacht29 <nacht29.study@gmail.com>
2025-08-26 19:25:54 +0800

    first commit
    - add access key
    - set up Py script for binance websocket

---
+++ websocket.py
@@ -0,0 +1,10 @@
+import asyncio
+import pandas as pd
+from binance import AsyncClient, BinanceSocketManager
+
+# set access key
+ACCESS_KEY = key_str[0]
+SECRET_ACCESS_KEY = key_str[1]
+
+async def main():
+	pass
