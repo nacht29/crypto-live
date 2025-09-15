308787ce6a2d9ba1015cd3c1d0519448383d6f43
nacht29 <nacht29.study@gmail.com>
2025-09-16 03:24:41 +0800

    fix: added parentheses for loop.time()

--- src/pipeline.py
+++ src/pipeline.py
@@ -15,8 +15,8 @@ BUCKET = "crypto-live-bucket"
 BATCH_JSONL_BUCKET_DIR = "batch_jsonl"
 
 # Processing
-MAX_BATCH_SIZE = 5 # change to 5000 for prod
-MAX_BATCH_TIMEOUT = 2 # wait 2 seconds for input or force flush (batch)
+MAX_BATCH_SIZE = 1000 # change to 5000 for prod
+MAX_BATCH_TIMEOUT = 3 # wait 2 seconds for input or force flush (batch)
 BATCH_JSONL_FILES = "/mnt/c/Users/Asus/Desktop/crypto-live/batch_json"
 
 # create boto3 session
@@ -51,7 +51,7 @@ async def batch_data(raw_queue:asyncio.Queue, batch_queue:asyncio.Queue, max_bat
 		nonlocal buffer, last_flush
 		# reset flush time if buffer is empty
 		if not buffer:
-			last_flush = loop.time
+			last_flush = loop.time()
 		# batch data
 		await batch_queue.put(buffer)
 		# reset flush state
