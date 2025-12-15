54e8f0845e5c8d177fcee49704ec76fe804d1157
nacht29 <nacht29.study@gmail.com>
2025-12-15 21:54:00 +0800

    chore: added comments for reference

--- docker/docker.sh
+++ docker/docker.sh
@@ -7,10 +7,13 @@ sudo docker push nacht29/crypto-live:latest
 
 # run image
 # can be done with 2 options
+
+# option 1
 sudo docker run --rm -it \
 	-e AWS_PROFILE=crypto-live-pipeline01 \
 	-v /home/nacht29/.aws/config:/root/.aws/config:ro \
 	-v /home/nacht29/.aws/credentials:/root/.aws/credentials:ro \
 	crypto-live
 
-sudo docker compose up .
+# option 2
+sudo docker compose up
